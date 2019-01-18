ac04a8d4e8106613b44cc81de764ac4a1ccc6a15
Justin Davis <davisjustinw@gmail.com>
2019-01-18 05:53:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,6 @@
+require 'pry'
 class Triangle
+  
   attr_accessor :side_a, :side_b, :side_c
   
   def initialize(sides_a, side_b, side_c)
@@ -8,6 +10,7 @@ class Triangle
   end 
   
   def kind 
+    binding.pry
     if side_a == side_b && side_b == side_c
       symbol = :equilateral
     elsif side_a != side_b && side_b != side_c
