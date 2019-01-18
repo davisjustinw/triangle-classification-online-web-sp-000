b08a6394674f92dede8d20ef666f5696f0450f80
Justin Davis <davisjustinw@gmail.com>
2019-01-18 05:52:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(sides_a, side_b, side_c)
+    @side_a = side_a 
+    @side_b = side_b
+    @side_c = side_c
+  end 
+  
+  def kind 
+    if side_a == side_b && side_b == side_c
+      symbol = :equilateral
+    elsif side_a != side_b && side_b != side_c
+      symbol = :scalene
+    else
+      symbol = :isosceles
+    end 
+    symbol
+  end
 end
